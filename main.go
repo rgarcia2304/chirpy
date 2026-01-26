6c84fd71eb97f1b0a389128ea798c4d6de3ef7d8
rgarcia2304 <garcia2304@icloud.com>
2026-01-26 16:17:57 -0500

    added more custom routing

--- main.go
+++ main.go
@@ -10,8 +10,14 @@ func main() {
 	const port = ":8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
-	mux.Handle("/assets", http.FileServer(http.Dir("./assets/logo.png")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	//mux.Handle("/assets", http.FileServer(http.Dir("/assets/")))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Ok"))
+	})
 
 	s := &http.Server{
 		Addr: port,
