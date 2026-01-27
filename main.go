15b6d1ba1c8cb9d807629ef1f2d88268c63dfd23
rgarcia2304 <garcia2304@icloud.com>
2026-01-27 09:54:39 -0500

    added stateful handlers

--- main.go
+++ main.go
@@ -1,16 +1,32 @@
 package main 
+
 import(
 	"net/http"
 	"log"
 	"fmt"
+	"sync/atomic"
+	"strconv"
 )
+type apiConfig struct{
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) requestsHandler(w http.ResponseWriter, r *http.Request){
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		message := "Hits: " + strconv.FormatInt(int64(cfg.fileserverHits.Load()), 10)
+		w.Write([]byte(message))
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = ":8080"
+	
+	//initialize the fileserverHits
+	apiCfg := apiConfig{}
 
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/", apiCfg.middlewareMetricInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot)))))
 	//mux.Handle("/assets", http.FileServer(http.Dir("/assets/")))
 
 	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request){
@@ -18,6 +34,8 @@ func main() {
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("Ok"))
 	})
+
+	mux.HandleFunc("/metrics", apiCfg.requestsHandler)
 
 	s := &http.Server{
 		Addr: port,
